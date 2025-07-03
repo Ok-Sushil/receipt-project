9567e850d6edca9ca11f26babc5b66bd9d829f0f
Ok-Sushil <sushil26saini@gmail.com>
2025-07-03 12:32:59 +0530

    its fixed the port issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const { execSync } = require('child_process');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
